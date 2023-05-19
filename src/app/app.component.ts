46095bcbce29932de1dc82a469db40ff881a4874
Victorvs1967 <victorsmirnov@me.com>
2023-05-19 22:14:01 +0300

    image cropper create

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ImageCroppedEvent } from 'ngx-image-cropper';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'angular-image-cropper-app';
+
+  imgChangeEvt: any = '';
+  cropImgPreview: any = '';
+
+  onFileChange(event: any): void {
+    this.imgChangeEvt = event;
+  }
+
+  cropImg(e: ImageCroppedEvent) {
+    this.cropImgPreview = e.base64;
+  }
+
+  imgLoad() {
+
+  }
+
+  initCropper() {
+
+  }
+
+  imgFailed() {
+
+  }
 }
